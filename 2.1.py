6279c4d9e50c23cc9a1df1215a48eb5e2ed22140
nikittua <vafela3337@gmail.com>
2022-11-18 19:19:56 +0300

    Построение выборок и эмперической функции распределения

---
+++ 2.1.py
@@ -0,0 +1,39 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.special import factorial
+import random
+from statsmodels.distributions.empirical_distribution import ECDF
+
+from scipy.stats import poisson
+
+
+
+
+
+# s = np.arange(0, 100)
+# pmf = poisson.pmf(s, mu=60)
+# print(pmf, 4)
+# plt.plot(s,pmf)
+# plt.show()
+
+
+mu = 21.5
+count = 1000
+r = poisson.rvs(mu, size=count)
+# plt.hist(r, bins=np.linspace(0, 35, 36), alpha=0.5, label='counting process', ec='black', align='left')
+# plt.plot(poisson.pmf(np.linspace(0, 35, 36),mu)*count)
+# plt.legend()
+# # plt.show()
+
+print(r)
+
+# Эмпирическая функция распределения
+# cdf = ECDF(r)
+# plt.plot(cdf.x, cdf.y, label="statmodels")
+# plt.legend()
+# plt.show()
+
+# x = np.sort(r)
+# y = np.arange(len(x))/float(len(x))
+# plt.plot(x, y)
+# plt.show()
