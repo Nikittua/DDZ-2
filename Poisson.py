25773764f040f63e390e1b6aaf992ef175c65b89
Nikittua <nisergeev_1@edu.hse.ru>
2022-11-20 00:20:26 +0300

    2.1 и 2.2 Распределений

--- Poisson.py
+++ Poisson.py
@@ -10,11 +10,7 @@ from scipy.stats import poisson
 
 
 
-# s = np.arange(0, 100)
-# pmf = poisson.pmf(s, mu=60)
-# print(pmf, 4)
-# plt.plot(s,pmf)
-# plt.show()
+
 
 
 mu = 21.5
@@ -23,15 +19,15 @@ r = poisson.rvs(mu, size=count)
 # plt.hist(r, bins=np.linspace(0, 35, 36), alpha=0.5, label='counting process', ec='black', align='left')
 # plt.plot(poisson.pmf(np.linspace(0, 35, 36),mu)*count)
 # plt.legend()
-# # plt.show()
+# plt.show()
 
 print(r)
 
 # Эмпирическая функция распределения
-# cdf = ECDF(r)
-# plt.plot(cdf.x, cdf.y, label="statmodels")
-# plt.legend()
-# plt.show()
+cdf = ECDF(r)
+plt.plot(cdf.x, cdf.y, label="statmodels")
+plt.legend()
+plt.show()
 
 # x = np.sort(r)
 # y = np.arange(len(x))/float(len(x))
